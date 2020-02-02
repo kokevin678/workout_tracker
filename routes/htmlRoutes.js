a820f31ef24ba3a20a9b616f256fb80e97c5a8e1
Chengyu Ko <kokevin678@gmail.com>
2020-02-01 18:41:53 -0800

    add htmlRoutes.hs

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = function(app, path) {
+  // send default page to all routes that are undefined
+  app.get("/exercise?", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  //index route loads index.html
+  app.get("/exercise", function(req, res) {
+    res.sendfile(path.join(__dirname, "../public/index.html"));
+  });
+
+  //stats route loads stats.html
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+// const db = require("./models");
+
+const app = express();
+
+// Routes
+require("./routes/htmlRoutes.js")(app);
+
+app.use(logger("dev"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set static root directory
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+  useNewUrlParser: true
+});
+
+// db.User.create({ name: "Ernest Hemingway" })
+//   .then(dbUser => {
+//     console.log(dbUser);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//   });
+
+// app.get("/notes", (req, res) => {
+//   db.Note.find({})
+//     .then(dbNote => {
+//       res.json(dbNote);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/user", (req, res) => {
+//   db.User.find({})
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
